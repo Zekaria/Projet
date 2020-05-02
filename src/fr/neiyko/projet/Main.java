31c60dc6dd51ba342193a9b3768d03677b89bf34
lirka.developpement@gmail.com <lirka.developpement@gmail.com>
2020-05-03 00:22:39 +0200

    Modif Main.java

--- src/fr/neiyko/projet/Main.java
+++ src/fr/neiyko/projet/Main.java
@@ -1,13 +1,14 @@
 package fr.neiyko.projet;
 
 import fr.neiyko.projet.commande.Alert;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     public void onEnable(){
-        System.out.println("§6Plugin Project : §4ON");
+        System.out.println(ChatColor.BOLD +"Plugin Project : ON");
         getCommand("alert").setExecutor((CommandExecutor)new Alert());
     }
 }
